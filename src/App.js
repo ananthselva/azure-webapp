fba1d66e009acb082c8fef0d1a344fcca6a4ffbc
ananthselva <ananthselva1998@gmail.com>
2024-02-09 11:43:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Foot from './foot-function/foot';
+
+function App() {
+  return (
+    <>
+    <Foot/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/foot-function/foot.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./footer.sass"
+export default function Foot() {
+  return (
+    <div>hello</div>
+  )
+}
